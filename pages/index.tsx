7f72ea8180f5c33509aed86df788fd99018efacd
TMNRP <tony.venom007@gmail.com>
2021-11-16 02:14:13 +0530

    Code refactored

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,134 @@
-import { Board } from "../components/Board";
+import { useCallback, useEffect, useState } from "react";
+import { motion, Variants } from "framer-motion";
 
 const Home = () => {
+  //
+  const [resetCounter, setResetCounter] = useState<number>(0);
+  const resetGame = useCallback(
+    () => setResetCounter((resetCounter) => ++resetCounter),
+    []
+  );
+
+  // generate tiles
+  const mode = 3;
+  const [tileNumbers, setTileNumbers] = useState(
+    Array.from(Array(mode * mode).keys())
+  );
+  useEffect(() => {
+    resetCounter > 0 &&
+      setTileNumbers([...Array.from(Array(mode * mode).keys())]);
+  }, [resetCounter]);
+
+  //
+  const [player, setPlayer] = useState<"cross" | "circle">("cross");
+  const togglePlayer = useCallback(
+    () => setPlayer((player) => (player === "cross" ? "circle" : "cross")),
+    []
+  );
+
+  //
+  const markSelected = useCallback(
+    (tileNumber: number) => {
+      const el = document.querySelector(`.tile-${tileNumber} .cell`);
+      const isSelected =
+        el?.classList.contains("cross") || el?.classList.contains("circle");
+
+      //
+      if (!isSelected) {
+        el?.classList.add(player);
+        el?.classList.remove(`${player}-hover`);
+        togglePlayer();
+      }
+    },
+    [player]
+  );
+
+  //
+  const updateTileType = useCallback(
+    (tileNumber: number) => {
+      const el = document.querySelector(`.tile-${tileNumber} .cell`);
+      const isSelected =
+        el?.classList.contains("cross") || el?.classList.contains("circle");
+
+      //
+      if (!isSelected) {
+        el?.classList.add(`${player}-hover`);
+        el?.classList.remove(
+          player === "cross" ? "circle-hover" : "cross-hover"
+        );
+      }
+    },
+    [player]
+  );
+
+  //
   return (
-    <div
-      className={`flex justify-center items-center h-screen bg-gradient-to-br from-blue-700 to-green-600 text-white`}
-    >
-      <Board mode={3} />
+    <div className="h-screen flex justify-center items-center bg-gradient-to-br from-blue-700 to-green-600 text-white">
+      <div className="flex flex-col justify-center items-center">
+        <motion.div
+          className={`flex justify-between items-center`}
+          variants={VStaggerContainer}
+          initial="initial"
+          animate="animate"
+        >
+          <motion.div variants={VStaggerItems} className="flex tracking-widest">
+            <p className="mr-2">Current player :</p>
+            <p className="uppercase font-semibold">{player}</p>
+          </motion.div>
+          <motion.button
+            variants={VStaggerItems}
+            className={`shadow-md m-3 px-3 py-1.5 rounded hover:shadow-lg text-white border border-green-300 hover:bg-green-500 tracking-widest`}
+            onClick={resetGame}
+          >
+            Reset
+          </motion.button>
+        </motion.div>
+
+        <motion.div
+          key={`key-${resetCounter}`}
+          className={`board grid grid-cols-3 gap-2`}
+          variants={VStaggerContainer}
+          initial="initial"
+          animate="animate"
+        >
+          {tileNumbers?.map((tileNumber) => (
+            <motion.div
+              key={`key-${tileNumber}`}
+              className={`tile tile-${tileNumber} w-20 h-20 flex justify-center items-center border border-green-300 rounded-sm relative`}
+              variants={VStaggerItems}
+            >
+              <div
+                className={`cell h-full w-full flex justify-center items-center cursor-pointer`}
+                onClick={() => markSelected(tileNumber)}
+                onMouseOver={() => updateTileType(tileNumber)}
+              />
+            </motion.div>
+          ))}
+        </motion.div>
+      </div>
     </div>
   );
 };
 
 export default Home;
+
+//
+const VStaggerContainer: Variants = {
+  animate: {
+    transition: {
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+export const VStaggerItems: Variants = {
+  initial: {
+    y: "-100vh",
+    opacity: 0,
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: { type: "spring", bounce: 0.2, duration: 0.6 },
+  },
+};
